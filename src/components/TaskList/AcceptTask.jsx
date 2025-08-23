72518b334dda6557571c8cab0ca35377ef0dc1b4
Ayush Walke <ayushwalke1@gmail.com>
2025-08-23 20:38:39 +0530

    make fields dynamic

--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 
-const AcceptTask = () => {
+const AcceptTask = ({data}) => {
+    console.log('task', data);
   return (
     <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl">
       <div className="flex justify-between items-center">
-        <h3 className="bg-red-600 px-3 py-1 rounded text-sm">High</h3>
-        <h4 className="text-sm">22 aug 2025</h4>
+        <h3 className="bg-red-600 px-3 py-1 rounded text-sm">{data.category}</h3>
+        <h4 className="text-sm">{data.date}</h4>
       </div>
-      <h2 className="mt-5 text-2xl font-semibold">Complete this project</h2>
+      <h2 className="mt-5 text-2xl font-semibold">{data.title}</h2>
       <p className="text-sm mt-2">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
-        temporibus veniam, eum enim nulla est!
+        {data.description}
       </p>
       <div className="flex justify-between mt-4">
-        <button className="bg-green-500 py-1 px-2 text-sm">
+        <button className="bg-green-500 py-1 px-2 text-sm rounded">
           Mark as Completed
         </button>
-        <button className="bg-red-500 py-1 px-2 text-sm">Mark as Failed</button>
+        <button className="bg-red-500 py-1 px-2 text-sm rounded">Mark as Failed</button>
       </div>
     </div>
   );
--- src/components/TaskList/NewTask.jsx
+++ src/components/TaskList/NewTask.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 
-const NewTask = () => {
+const NewTask = ({data}) => {
   return (
     <div className="flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl">
       <div className="flex justify-between items-center">
-        <h3 className="bg-red-600 px-3 py-1 rounded text-sm">High</h3>
-        <h4 className="text-sm">22 aug 2025</h4>
+        <h3 className="bg-red-600 px-3 py-1 rounded text-sm">{data.category}</h3>
+        <h4 className="text-sm">{data.date}</h4>
       </div>
-      <h2 className="mt-5 text-2xl font-semibold">Complete this project</h2>
+      <h2 className="mt-5 text-2xl font-semibold">{data.title}</h2>
       <p className="text-sm mt-2">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
-        temporibus veniam, eum enim nulla est!
+        {data.description}
       </p>
       <div className="mt-4">
         <button>Accept Task</button>
