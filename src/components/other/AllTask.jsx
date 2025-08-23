f9b8616d37163e4f378e71a02226d3facb9c0255
Ayush Walke <ayushwalke1@gmail.com>
2025-08-23 23:35:18 +0530

    show task update on admin dashboard

--- src/components/other/AllTask.jsx
+++ src/components/other/AllTask.jsx
@@ -1,35 +1,31 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AuthContext } from "../../context/AuthProvider";
 
 const AllTask = () => {
+  const authData = useContext(AuthContext);
+  console.log(authData.employees);
   return (
-    <div className='bg-[#1C1C1C] p-5 mt-5 rounded h-80 overflow-auto'>
-        <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2>All tasks</h2>
-            <h3>complete this project</h3>
-            <h5>Status</h5>
-        </div>
-        <div className='bg-green-400 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2>All tasks</h2>
-            <h3>complete this project</h3>
-            <h5>Status</h5>
-        </div>
-        <div className='bg-yellow-400 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2>All tasks</h2>
-            <h3>complete this project</h3>
-            <h5>Status</h5>
-        </div>
-        <div className='bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2>All tasks</h2>
-            <h3>complete this project</h3>
-            <h5>Status</h5>
-        </div>
-        <div className='bg-purple-400 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2>All tasks</h2>
-            <h3>complete this project</h3>
-            <h5>Status</h5>
-        </div>
+    <div className="bg-[#1C1C1C] p-5 mt-5 rounded h-80">
+      <div className="bg-[#3F3F3F] mb-2 py-2 px-4 flex justify-between rounded text-gray-200 font-semibold">
+        <h2 className="text-xl font-medium w-1/5">Employee Name</h2>
+        <h3 className="text-xl font-medium w-1/5">New tasks</h3>
+        <h5 className="text-xl font-medium w-1/5">Active tasks</h5>
+        <h5 className="text-xl font-medium w-1/5">Completed tasks</h5>
+        <h5 className="text-xl font-medium w-1/5">Failed tasks</h5>
+      </div>
+      <div className="h-[80%] overflow-auto">
+        {authData.employees.map((elem) => (
+          <div className="bg-[#2D2D2D] mb-2 py-2 px-4 flex justify-between rounded text-gray-200">
+            <h2 className="text-xl font-medium w-1/5">{elem.firstName}</h2>
+            <h3 className="text-xl font-medium w-1/5 text-sky-400">{elem.taskSummary.newTask}</h3>
+            <h5 className="text-xl font-medium w-1/5 text-yellow-400">{elem.taskSummary.active}</h5>
+            <h5 className="text-xl font-medium w-1/5 text-green-500">{elem.taskSummary.completed}</h5>
+            <h5 className="text-xl font-medium w-1/5 text-red-500">{elem.taskSummary.failed}</h5>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AllTask
+export default AllTask;
