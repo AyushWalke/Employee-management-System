5e43e4795b6f61fa70807e0534fffc03388629ad
Ayush Walke <ayushwalke1@gmail.com>
2025-08-23 20:38:52 +0530

    pass data

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -11,18 +11,18 @@ const TaskList = ({ data }) => {
       id="tasklist"
       className="h-[40%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap py-5 w-full mt-10"
     >
-      {data.tasks.map((elem)=>{
+      {data.tasks.map((elem, idx)=>{
        if(elem.active){
-        return <AcceptTask />
+        return <AcceptTask key={idx} data={elem}/>
        }
        if(elem.newTask){
-        return <NewTask />
+        return <NewTask key={idx} data={elem}/>
        }
        if(elem.completed){
-        return <CompleteTask />
+        return <CompleteTask key={idx} data={elem}/>
        }
        if(elem.failed){
-        return <FailedTask />
+        return <FailedTask key={idx} data={elem}/>
        }
       })}
     </div>
