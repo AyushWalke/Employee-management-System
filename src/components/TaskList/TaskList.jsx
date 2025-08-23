aa1606fb7aec4c3777254bbde9be16f41167945a
Ayush Walke <ayushwalke1@gmail.com>
2025-08-23 17:48:24 +0530

    conditionally rendering tasks

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,46 +1,32 @@
-import React from 'react'
+import React from "react";
+import AcceptTask from "./AcceptTask";
+import NewTask from "./NewTask";
+import CompleteTask from "./CompleteTask";
+import FailedTask from "./FailedTask";
 
-const TaskList = () => {
+const TaskList = ({ data }) => {
+    console.log(data)
   return (
-    <div id='tasklist' className='h-[40%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap py-5 w-full mt-10'>
-        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl'>
-            <div className='flex justify-between items-center'>
-                <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
-                <h4 className='text-sm'>22 aug 2025</h4>
-            </div>
-            <h2 className='mt-5 text-2xl font-semibold'>Complete this project</h2>
-            <p className='text-sm mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis temporibus veniam, eum enim nulla est!</p>
-        </div>
-        
-        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl'>
-            <div className='flex justify-between items-center'>
-                <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
-                <h4 className='text-sm'>22 aug 2025</h4>
-            </div>
-            <h2 className='mt-5 text-2xl font-semibold'>Complete this project</h2>
-            <p className='text-sm mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis temporibus veniam, eum enim nulla est!</p>
-        </div>
-
-        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-green-400 rounded-xl'>
-            <div className='flex justify-between items-center'>
-                <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
-                <h4 className='text-sm'>22 aug 2025</h4>
-            </div>
-            <h2 className='mt-5 text-2xl font-semibold'>Complete this project</h2>
-            <p className='text-sm mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis temporibus veniam, eum enim nulla est!</p>
-        </div>
-
-        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-yellow-400 rounded-xl'>
-            <div className='flex justify-between items-center'>
-                <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
-                <h4 className='text-sm'>22 aug 2025</h4>
-            </div>
-            <h2 className='mt-5 text-2xl font-semibold'>Complete this project</h2>
-            <p className='text-sm mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis temporibus veniam, eum enim nulla est!</p>
-        </div>
-
+    <div
+      id="tasklist"
+      className="h-[40%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap py-5 w-full mt-10"
+    >
+      {data.tasks.map((elem)=>{
+       if(elem.active){
+        return <AcceptTask />
+       }
+       if(elem.newTask){
+        return <NewTask />
+       }
+       if(elem.completed){
+        return <CompleteTask />
+       }
+       if(elem.failed){
+        return <FailedTask />
+       }
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default TaskList
+export default TaskList;
