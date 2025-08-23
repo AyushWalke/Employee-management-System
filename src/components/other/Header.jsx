b3fa3dfe30ae0328f85856e09273d3ef7317aeab
Ayush Walke <ayushwalke1@gmail.com>
2025-08-23 20:15:51 +0530

    show username and make logout btn work

--- src/components/other/Header.jsx
+++ src/components/other/Header.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-const Header = ({data}) => {
-  console.log(data);
+const Header = ({ data }) => {
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    if (!data) {
+      setUsername("Admin");
+    } else {
+      setUsername(data.firstName);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.setItem('loggedInUser', '');
+    window.location.reload();
+  }
+
   return (
-    <div className='flex items-center justify-between'>
-        <h1 className='text-2xl font-medium'>Hello <br/> <span className='text-3xl font-semibold'>{data.firstName}👋</span></h1>
-        <button className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded '>Log out</button>
+    <div className="flex items-center justify-between">
+      <h1 className="text-2xl font-medium">
+        Hello <br />{" "}
+        <span className="text-3xl font-semibold">{username}👋</span>
+      </h1>
+      <button onClick={handleLogout} className="bg-red-600 text-lg font-medium text-white px-5 py-2 rounded ">
+        Log out
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
