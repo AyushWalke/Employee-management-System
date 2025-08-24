e12599658149a154eeb793f997ac60a58454f970
Ayush Walke <ayushwalke1@gmail.com>
2025-08-24 18:31:31 +0530

    change bg color and design btn

--- src/components/TaskList/NewTask.jsx
+++ src/components/TaskList/NewTask.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NewTask = ({data}) => {
   return (
-    <div className="flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl">
+    <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-400 rounded-xl">
       <div className="flex justify-between items-center">
         <h3 className="bg-red-600 px-3 py-1 rounded text-sm">{data.category}</h3>
         <h4 className="text-sm">{data.date}</h4>
@@ -12,7 +12,7 @@ const NewTask = ({data}) => {
         {data.description}
       </p>
       <div className="mt-4">
-        <button>Accept Task</button>
+        <button className="bg-blue-600 px-2 rounded">Accept Task</button>
       </div>
     </div>
   );
